11513cdeb5e02e7bc0ca55f9baaec23d5c0770f3
Micah Cowell <micahcowell@gmail.com>
2025-04-05 16:40:02 -0700

    login page

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -145,10 +145,12 @@ func main() {
 
 func generateSessionToken() (string, error) {
 	b := make([]byte, 32)
+
 	_, err := rand.Read(b)
 	if err != nil {
 		return "", err
 	}
+
 	return hex.EncodeToString(b), nil
 }
 
