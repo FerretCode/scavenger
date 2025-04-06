7c0a0fef1ae9d29430a0f07eccc4ea6710e96e38
ayushpdx05 <ayush05px@gmail.com>
2025-04-05 20:08:25 -0700

    reverse proxy installed

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,14 +8,16 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
+	"structs"
 	"sync"
 	"text/template"
-	"os"
 	"time"
 
 	"github.com/ferretcode/scavenger/internal/auth"
 	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 	"go.mongodb.org/mongo-driver/v2/mongo/readpref"
@@ -38,6 +40,11 @@ func parseTemplates() error {
 	}
 
 	return nil
+}
+
+type WorkFlow struct {
+	WorkFlowId          string `bson:"work_flow_id"`
+	ContainerServiceUri string `bson:"container_service_uri"`
 }
 
 func main() {
@@ -91,6 +98,45 @@ func main() {
 		})
 	})
 
+	// workflowcollections.findOne()
+	// workflowcollections.findOne()
+
+	// retriveing container id
+	// workFlowCollections = db.collections('workflows')
+	// var workflow := workflow{}
+	// id := req.
+	// filter := bson.d{'id':id}o
+	// res := workflowcollections.findOne(conteext param,filter,)
+	// workdflow = res.decode
+	r.Get("/connect/{work_flow_id}", func(w http.ResponseWriter, r *http.Request) {
+		//connect to proxy
+		workFlowId := chi.URLParam(r, "work_flow_id")
+		// 5000
+		filter := bson.D{{"id", workFlowId}}
+		res := client.Database("scavenger").Collection("workflows").FindOne(ctx, filter)
+		decodedWorkFlow := WorkFlow{}
+
+		if res.Err() != nil {
+			handleError(res.Err(), w, "connect")
+			return
+		}
+		//workfow is a structu which will be populated
+
+		err := res.Decode(&decodedWorkFlow)
+		if err != nil {
+			handleError(err, w, "connect")
+			return
+		}
+
+		proxy, err := connectingHostToUser(decodedWorkFlow.WorkFlowId)
+		if err != nil {
+			handleError(err, w, "connect")
+			return
+		}
+
+		proxy.ServeHTTP(w, r)
+	})
+
 	r.Route("/auth", func(r chi.Router) {
 		r.Get("/login", func(w http.ResponseWriter, r *http.Request) {
 			handleError(auth.RenderLogin(w, r, templates), w, "login/render")
@@ -119,18 +165,37 @@ func handleError(err error, w http.ResponseWriter, svc string) {
 	}
 }
 
-//taking the host req
-//creatign a reverse proxy using httputil
-func connectingHostToUser(hostString string)(*httputil.ReverseProxy,error){
+// taking the host req
+// creatign a reverse proxy using httputil
+func connectingHostToUser(hostString string) (*httputil.ReverseProxy, error) {
 	url, err := url.Parse(hostString)
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 	return httputil.NewSingleHostReverseProxy(url), nil
 }
 
-func proxyRequestHandler(proxy *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request){
-		return func(w http.ResponseWriter, r *http.Request){
-				proxy.ServeHTTP(w,r)
-		}		
+func proxyRequestHandler(proxy *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		proxy.ServeHTTP(w, r)
+	}
+}
+
+//connnect to the scavenger database , workflow collections
+// find that specificic id
+// decode acess that specific workflow id
+
+// retriveing container id
+// database = client.connect('scavanehger')
+// workFlowCollections = db.collections('workflows')
+// var workflow := workflow{}
+// id := req.
+// filter := bson.d{'id':id}o
+// res := workflowcollections.findOne(conteext param,filter,)
+// workdflow = res.decode
+// given container url employ the reverse proxy handler
+
+// retrive the containerid from the mongodb database
+func retriveContainerIdFromDataBase() {
+
 }
