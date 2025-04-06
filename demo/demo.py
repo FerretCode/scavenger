5bf2a51b4a073d33102957d703e4594f9b8b89d6
ferretcode <eggertet@oregonstate.edu>
2025-04-06 12:32:24 -0700

    clean

--- demo/demo.py
+++ demo/demo.py
@@ -2,15 +2,19 @@ import os
 import asyncio
 import websockets
 import json
+from dotenv import load_dotenv
 from datetime import datetime
+
+load_dotenv()
+
 
 async def websocket_client():
     uri = "ws://127.0.0.1:3000/connect/debt"
     headers = {
-        "X-API-Key": os.environ['API_KEY'],
+        "X-API-Key": os.environ['SCAVENGER_API_KEY'],
     }
 
-    async with websockets.connect(uri, additional_headers=headers) as websocket:        
+    async with websockets.connect(uri, additional_headers=headers) as websocket:
         try:
             while True:
                 # Receive a message from the server
